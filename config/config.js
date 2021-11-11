57541b97f77eb4fbb67be70250fbdf0b76cb1782
emrizki <emrizkidev@gmail.com>
2021-11-11 18:07:26 +0700

    add env example

---
+++ config/config.js
@@ -0,0 +1,34 @@
+const env = process.env.NODE_ENV || 'development';
+const upperCaseEnv = env.toUpperCase();
+
+if (process.env.NODE_ENV === 'development') require('dotenv').config();
+
+const username = process.env['DB_USERNAME_' + upperCaseEnv];
+const password = process.env['DB_PASSWORD_' + upperCaseEnv];
+const database = process.env['DB_NAME_' + upperCaseEnv];
+const host = process.env['DB_HOST_' + upperCaseEnv];
+const dialect = process.env['DB_DIALECT_' + upperCaseEnv];
+
+module.exports = {
+  development: {
+    username,
+    password,
+    database,
+    host,
+    dialect,
+  },
+  test: {
+    username: 'root',
+    password: null,
+    database: 'database_test',
+    host: '127.0.0.1',
+    dialect: 'mysql',
+  },
+  production: {
+    username: 'root',
+    password: null,
+    database: 'database_production',
+    host: '127.0.0.1',
+    dialect: 'mysql',
+  },
+};
