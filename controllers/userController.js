728fedbe648b195771fb95e50099e95ed517e5bb
almas615 <almasadlilwafi615@gmail.com>
2021-11-12 10:52:26 +0700

    tambah fungsi findOne

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,8 +1,26 @@
 const {User} = require('../models');
 
 
-
+const findOne = (req,res)=>{
+    User.findAll({
+        where:{
+            id:req.params.id
+        }
+    })
+    .then(data =>{
+        res.status(200).json({
+            result:"success",
+            data: data
+        });
+    })
+    .catch(err =>{
+        res.status(500).json({
+            result:"failed",
+            message: err.message || "some error occured while retrieving game"
+        })
+    })
+}
 
 module.exports ={
-    
+    findOne
 }
