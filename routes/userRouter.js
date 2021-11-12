16aa3153c14b54ff213ca1390105671d3873f77b
almas615 <almasadlilwafi615@gmail.com>
2021-11-12 14:52:59 +0700

    edit user router

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const user = require('../controllers/userController.js');
 
+router.get('/leaderboard', user.getLeaderboard);
 router.get('/:id',user.findOne);
 
 module.exports = router;
