972d0eaccec0e60cb9d41471d90ab7caa8b8be99
almas615 <almasadlilwafi615@gmail.com>
2021-11-12 10:52:45 +0700

    tambah route /user

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
 const authRouter = require('./authRouter');
 const game = require('./gameRouter');
+const user = require('./userRouter');
 
 router.use('/', authRouter);
 router.use('/game', game);
+router.use('/user', user);
 
 module.exports = router;
