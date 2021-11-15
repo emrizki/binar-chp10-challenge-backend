798a358b837d8a2ae74c994a34814f5aa7f66efa
uilmi <uilmi@na.edu>
2021-11-15 21:03:41 +0700

    ENV

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const swaggerUI = require('swagger-ui-express');
 
 
 const swaggerJSON = require('./swagger.json');
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
