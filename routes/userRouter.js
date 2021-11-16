35584635d11198c57269e231730463bdac1ad501
almas615 <almasadlilwafi615@gmail.com>
2021-11-16 20:45:10 +0700

    edit urutan router

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -4,12 +4,11 @@ const authorization = require('../middlewares/authorization');
 const userController = require('../controllers/userController');
 
 
-router.put('/:id/score', userController.updateScore);
 router.use(restrict);
 router.get('/', userController.getAllUser);
-router.put('/update/:id', authorization, userController.updateUser);
-router.get('/leaderboard', userController.getLeaderboard);
 router.get('/:id',userController.findOne);
+router.put('/update/:id', authorization, userController.updateUser);
+router.put('/:id/score', userController.updateScore);
 
 
 module.exports = router;
