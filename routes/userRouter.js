bd41cb6e78c8bf3e33e611a9554c731029f9ef92
almas615 <86828635+almas615@users.noreply.github.com>
2021-11-17 21:52:43 +0700

    get user by username tanpa authentication

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -3,10 +3,9 @@ const restrict = require('../middlewares/restrict');
 const authorization = require('../middlewares/authorization');
 const userController = require('../controllers/userController');
 
-
+router.get('/:username',userController.findOne);
 router.use(restrict);
 router.get('/', userController.getAllUser);
-router.get('/:username',userController.findOne);
 router.put('/update/:id', authorization, userController.updateUser);
 router.put('/:id/score', userController.updateScore);
 
