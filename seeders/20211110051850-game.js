240e39f7256d1414e02902c16ac3d514b3281ef1
almas615 <almasadlilwafi615@gmail.com>
2021-12-08 21:44:25 +0700

    tambah game seeder

--- seeders/20211110051850-game.js
+++ seeders/20211110051850-game.js
@@ -15,14 +15,33 @@ module.exports = {
     await queryInterface.bulkInsert('games', [
       {
       name: 'batu-kertas-guntin',
+      description: 'permainan batu kertas gunting ',
+      play_count: 100,
       createdAt: new Date(),
       updatedAt: new Date()
     },
     {
       name:'monopoli',
+      description: 'permainan monopoli',
+      play_count: 50,
       createdAt: new Date(),
       updatedAt: new Date()
-    }
+    },
+    {
+      name:'congklak',
+      description: 'permainan congklak',
+      play_count: 20,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },
+    {
+      name:'tebak angka',
+      description: 'permainan tebak angka',
+      play_count: 30,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },
+
   ], {});
   },
 
